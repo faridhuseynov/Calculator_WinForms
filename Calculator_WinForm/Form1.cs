6a3006959d877136120da14d3decf81d921a07c1
faridhuseynov <35901226+faridhuseynov@users.noreply.github.com>
2018-08-09 23:53:30 +0400

    Updated function to show the number

--- Calculator_WinForm/Form1.cs
+++ Calculator_WinForm/Form1.cs
@@ -12,7 +12,9 @@ namespace Calculator_WinForm
 {
     public partial class Form1 : Form
     {
-        List<string> number = new List<string>();
+        StringBuilder number = new StringBuilder();
+        StringBuilder _operator = new StringBuilder();
+        int result = 0;
         public Form1()
         {
             InitializeComponent();
@@ -22,11 +24,19 @@ namespace Calculator_WinForm
         private void OnButtonClick(object sender, EventArgs e)
         {
             var btn=sender as Button;
-            number.Add(btn.Text);
-            textBox.Text=number.;
+            number.Append(btn.Text);
+            textBox.Text += btn.Text;
             textBox.Show();
         }
 
-       
+        private void OnOperatorClick(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (_operator.Length != 0)
+                _operator.Clear();
+            _operator.Append(btn.Text);
+
+            number.Clear();
+        }
     }
 }
